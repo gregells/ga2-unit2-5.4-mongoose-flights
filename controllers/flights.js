3c2553517b48595e4cfce9d33dc815c9740a48d3
Greg Ells <gregells@gmail.com>
2025-01-31 16:29:04 -0500

    add controller action for creating new flight

--- controllers/flights.js
+++ controllers/flights.js
@@ -3,6 +3,7 @@ const Flight = require('../models/flight');
 module.exports = {
   index,
   new: newFlight,
+  create,
 }
 
 async function index (req, res) {
@@ -17,4 +18,17 @@ function newFlight (req, res) {
   res.render('flights/new', {
     title: 'Add New Flight'
   });
+}
+
+async function create (req, res) {
+  try {
+    await Flight.create(req.body);
+    res.redirect('/flights');
+  } catch (err) {
+    console.log(err);
+    res.render('flights/new', { 
+      title: 'Add New Flight',
+      errorMsg: err.messge
+    });
+  }
 }

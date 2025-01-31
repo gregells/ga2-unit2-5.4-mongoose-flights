60b310fff2a2306a28567c8b19a8bbb5cfab2bd3
Greg Ells <gregells@gmail.com>
2025-01-31 16:28:50 -0500

    add post route for creating new flight

--- routes/flights.js
+++ routes/flights.js
@@ -6,5 +6,7 @@ const flightsCtrl = require('../controllers/flights');
 router.get('/', flightsCtrl.index);
 // GET /flights/new
 router.get('/new', flightsCtrl.new);
+// POST /flights
+router.post('/', flightsCtrl.create);
 
 module.exports = router;

768cf8109d15b7d6b5dfb38d7ae509ca8cd2d9a5
Greg Ells <gregells@gmail.com>
2025-02-02 16:06:16 -0500

    add comment on starts-with path

--- routes/flights.js
+++ routes/flights.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 const flightsCtrl = require('../controllers/flights');
 
+// All routes start with /flights
+
 // GET /flights
 router.get('/', flightsCtrl.index);
 // GET /flights/new

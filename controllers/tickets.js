d4599bdf135d53aa0ac4c95597e78eddb29d8f51
Greg Ells <gregells@gmail.com>
2025-02-03 14:55:35 -0500

    add controller action to create a new ticket

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -3,6 +3,7 @@ const Flight = require('../models/flight');
 
 module.exports = {
   new: newTicket,
+  create,
 };
 
 function newTicket(req, res) {
@@ -11,4 +12,19 @@ function newTicket(req, res) {
     id: req.params.id,
     errorMsg: '',
   });
+}
+
+async function create(req, res) {
+  try {
+    req.body.flight = req.params.id;
+    await Ticket.create(req.body);
+    res.redirect(`/flights/${req.params.id}`);
+  } catch(err) {
+    console.log(err);
+    res.render('tickets/new', { 
+      title: 'Add New Flight',
+      id: req.params.id,
+      errorMsg: err.message,
+    });
+  }
 }
